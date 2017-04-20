878d71820dcee270feee8297db7a0770de57c25d
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-04-20 11:21:58 +0400

    Addition

---
+++ OperationsWithFiles/OperationsWithFiles/Controllers/FileController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OperationsWithFiles.Controllers
+{
+    public class FileController : ApiController
+    {
+        // GET: api/File
+        public IEnumerable<string> Get()
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"C:\Testing");
+            var files = directory.GetFiles().Where(f => !f.Attributes.HasFlag(FileAttributes.Hidden)).Select(f => f.Name).ToArray();
+            return files;
+        }
+
+        // GET: api/File/5
+        public string Get(string name)
+        {
+            string path = Path.Combine(@"C:\Testing", name);
+            StreamReader sr = File.OpenText(path);
+            string textline = sr.ReadLine();
+            sr.Close();
+            return textline;
+            // bad practice //
+            //HttpResponseMessage response = new HttpResponseMessage();
+            //response.Content = new StringContent(textline);
+            //return response;
+        }
+
+        // POST: api/File
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/File/5
+        public void Put(string name, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/File/5
+        public void Delete(string name)
+        {
+            string fileName = Path.Combine(@"C:\Testing", name);
+
+            string[] files = Directory.GetFiles(@"C:\Testing");
+
+            foreach (string file in files)
+            {
+                if (file.ToUpper().Contains(fileName.ToUpper()))
+                {
+                    File.Delete(file);
+                }
+            }
+        }
+    }
+}
---
+++ OperationsWithFiles/WPF-Client/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WPF_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GetAllFiles_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+
+            client.GetAsync(@"http://localhost:58370/api/directory")
+                .ContinueWith(response =>
+                {
+                    if (response.Exception != null)
+                    {
+                        MessageBox.Show(response.Exception.Message);
+                    }
+                    else
+                    {
+                        //System.Threading.Thread.Sleep(2000);
+                        HttpResponseMessage message = response.Result;
+                        string responseText = message.Content.ReadAsStringAsync().Result;
+
+                        // converts Json into CS //
+                        JavaScriptSerializer jss = new JavaScriptSerializer();
+                        ObservableCollection<string> files = jss.Deserialize<ObservableCollection<string>>(responseText);
+
+                        Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                            (Action)(() => {
+                                listOfFiles.DataContext = files;
+
+                                Binding binding = new Binding();
+                                listOfFiles.SetBinding(ItemsControl.ItemsSourceProperty, binding);
+
+                                (listOfFiles.ItemsSource as ObservableCollection<string>).RemoveAt(0);
+                            }));
+                    }
+                });
+
+        }
+
+        private void buttonGetByName_Click(object sender, RoutedEventArgs e)
+        {
+
+            HttpClient client = new HttpClient();
+
+            // URI-i mej vorpes parameter input e anum textBoxFileName-i mej grvac selectiony //
+            string url = string.Format("http://localhost:58370/api/directory?name={0}", Uri.EscapeDataString(textBoxFileName.Text));
+
+            client.GetAsync(url)
+                .ContinueWith(response =>
+                {
+                    if (response.Exception != null)
+                    {
+                        MessageBox.Show(response.Exception.Message);
+                    }
+                    else
+                    {
+                        //System.Threading.Thread.Sleep(2000);
+                        HttpResponseMessage message = response.Result;
+                        string responseText = message.Content.ReadAsStringAsync().Result;
+
+                        JavaScriptSerializer jss = new JavaScriptSerializer();
+                        string body = jss.Deserialize<string>(responseText);
+
+                        Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                            (Action)(() => { textBoxFileContent.Text = body; }));
+                    }
+                });
+
+
+        }
+
+        private void selectionEvent(object sender, SelectionChangedEventArgs e)
+        {
+            textBoxFileName.Text = listOfFiles.SelectedItem.ToString();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            string url = string.Format("http://localhost:58370/api/directory?name={0}", Uri.EscapeDataString(textBoxFileName.Text));
+
+            client.DeleteAsync(url);
+
+        }
+
+        
+    }
+}
